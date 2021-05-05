af207d54597c568af18e11dd33723f6ee963818d
Sandra <sandriniia_grebentcova@riseup.net>
2021-05-05 14:49:34 +0200

    fix MainPage styles

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -6,7 +6,6 @@ import '../../fonts/fonts.css';
 
 import search_loupe from '../../images/loupe.png';
 import spider_man_main_page from '../../images/spider-man.png';
-// import bats from '../../images/bat.png';
 
 function MainPage() {
   const [searchValue, setSearchValue] = useState(' ');
@@ -31,21 +30,22 @@ function MainPage() {
 
   return (
     <div className='main'>
-      <h1 className='main__title'>FIND YOUR HERO</h1>
-      <form className='main__search-form'>
-        <input
-          className='main__search-input'
-          type='text'
-          placeholder='Search'
-          autoFocus
-          onChange={searchValueHandler}
-        />
-        <Link to={`/profile/${id}`} className='main__button-search' type='submit'>
-          <img className='main__button-search-img' src={search_loupe} alt='Search loupe' />
-        </Link>
-      </form>
+      <div className='main__container'>
+        <h1 className='main__title'>FIND YOUR HERO</h1>
+        <form className='main__search-form'>
+          <input
+            className='main__search-input'
+            type='text'
+            placeholder='Search'
+            autoFocus
+            onChange={searchValueHandler}
+          />
+          <Link to={`/profile/${id}`} className='main__button-search' type='submit'>
+            <img className='main__button-search-img' src={search_loupe} alt='Search loupe' />
+          </Link>
+        </form>
+      </div>
       <img className='main__image' src={spider_man_main_page} alt='Spider-man'></img>
-      {/* <img className='main__image main__image-bats' src={bats} alt='Batman and Batwomen'></img> */}
     </div>
   );
 }
