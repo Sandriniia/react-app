46a8b1d731fdbbc9e9b1ab7e43b642a0fcaaec01
Sandra <sandriniia_grebentcova@riseup.net>
2021-05-15 23:09:04 +0200

    fix Routes path

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
               <Route path='/profile/:id/comics'>
                 <ComicsPage />
               </Route>
-              <Route path='/:'>
+              <Route path='/marvel-heroes-react-app'>
                 <MainPage />
               </Route>
             </Switch>
